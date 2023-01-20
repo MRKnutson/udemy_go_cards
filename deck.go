07f9f5c0d850f2031c724d623b7af01f3902925f
Michael Knutson <mrknutson44@gmail.com>
2023-01-20 10:08:03 -0900

    deal function

--- deck.go
+++ deck.go
@@ -24,3 +24,7 @@ func (d deck) printDeck() {
 		fmt.Println(i, card)
 	}
 }
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 func main() {
 	cards := newDeck()
 
-	cards.printDeck()
+	hand, remainingDeck := deal(cards, 5)
+
+	remainingDeck.printDeck()
+	hand.printDeck()
 
 }
